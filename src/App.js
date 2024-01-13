8baeb670b4a6fd1cac44ce6cdf5b695fd5363f94
gitrubio <carloviloria0@gmail.com>
2024-01-13 18:17:31 -0500

    add name

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import Work from './components/Work';
 import Contact from './components/Contact';
 
 const App = () => {
+  
   return (
     <div className='bg-site bg-no-repeat bg-cover overflow-hidden'>
       <Header />
