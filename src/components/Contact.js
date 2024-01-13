cd8a46258e7ab11b80aede376d079df6aa51cff5
gitrubio <carloviloria0@gmail.com>
2024-01-13 11:42:07 -0500

    finished portofolio based

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,7 +1,64 @@
-import React from 'react';
+import React from "react";
+//motion
+import { motion } from "framer-motion";
+//variants
+import { fadeIn } from "../variants";
 
 const Contact = () => {
-  return <div  id='contact' className='section' >Contact</div>;
+  return (
+    <section id="contact" className="py-16 lg:section">
+      <div className="container mx-auto">
+        <div className="flex flex-col lg:flex-row">
+          {/**  text */}
+          <motion.div
+            variants={fadeIn("right", 0.3)}
+            initial="hidden"
+            whileInView={"show"}
+            viewport={{ once: false, amount: 0.7 }}
+            className="flex-1 flex justify-start items-center"
+          >
+            <div>
+              <h4 className="text-xl uppercase text-accent font-medium mb-2 tracking-wide">
+                Get in touch
+              </h4>
+              <h2 className="text-[45px] lg:text-[90px] leading-none mb-12">
+                Let's work
+                <br /> together!
+              </h2>
+            </div>
+          </motion.div>
+          {/**  form */}
+          <motion.form
+            variants={fadeIn("left", 0.3)}
+            initial="hidden"
+            whileInView={"show"}
+            viewport={{ once: false, amount: 0.7 }}
+            className="flex-1 border rounded-2xl flex flex-col gap-y-6 pb-5 p-6 items-end"
+          >
+            <input
+              className="bg-transparent border-b py-3 outline-none w-full placeholder:text-white focus:border-accent transition-all"
+              type="text"
+              placeholder="Your name"
+            />
+            <input
+              className="bg-transparent border-b py-3 outline-none w-full placeholder:text-white focus:border-accent transition-all"
+              type="text"
+              placeholder="Your email"
+            />
+            <textarea
+              name=""
+              placeholder="Your message"
+              id=""
+              cols={30}
+              rows={3}
+              className="bg-transparent border-b py-3 outline-none w-full placeholder:text-white focus:border-accent transition-all resize-none mb-12"
+            ></textarea>
+            <button className="btn btn-lg">Send message</button>
+          </motion.form>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Contact;
--- src/components/Work.js
+++ src/components/Work.js
@@ -1,7 +1,106 @@
-import React from 'react';
+import React from "react";
+//motion
+import { motion } from "framer-motion";
+// variants
+import { fadeIn } from "../variants";
+// img
+import Img1 from "../assets/portfolio-img1.png";
+import Img2 from "../assets/portfolio-img2.png";
+import Img3 from "../assets/portfolio-img3.png";
 
 const Work = () => {
-  return <div id='work' className='section'>Work</div>;
+  return (
+    <section id="work" className="section">
+      <div className="container mx-auto">
+        <div className="flex flex-col lg:flex-row gap-x-10">
+          <motion.div
+            variants={fadeIn("right", 0.2)}
+            initial="hidden"
+            whileInView={"show"}
+            viewport={{ once: false, amount: 0.7 }}
+            className="flex-1 flex flex-col gap-y-12 mb-10 lg:mb-0"
+          >
+            {/**  text */}
+            <div>
+              <h2 className="h2  leading-tight text-accent">
+                {" "}
+                My lastest <br /> Work.
+              </h2>
+              <p className="max-w-sm mb-4">
+                Ad adipisicing mollit tempor ullamco fugiat dolor occaecat. Est
+                voluptate commodo in in in dolore amet commodo aliquip.
+                Consequat est incididunt amet et proident reprehenderit
+              </p>
+              <button className="btn btn-sm">View all projects</button>
+            </div>
+            {/**  image */}
+            <div className="group relative overflow-hidden border-2 border-white/50  rouderd-xl">
+              {/**  overloy */}
+              <div className="group-hover:bg-black/70 w-full h-full absolute z-40 transition-all duration-300"></div>
+              {/**  image */}
+              <img
+                className="group-hover:scale-125 transition-all duration-500"
+                src={Img2}
+                alt=""
+              />
+              {/**  pretitle */}
+              <div className="absolute  -bottom-full left-12 group-hover:bottom-24 transition-all duration-500  z-50">
+                <span className="text-gradient">UI/UX Desing</span>
+              </div>
+              {/**  title */}
+              <div className="absolute  -bottom-full left-12 group-hover:bottom-14 transition-all duration-700  z-50">
+                <span className="text-3xl text-white">Project Title</span>
+              </div>
+            </div>
+          </motion.div>
+          <motion.div
+            variants={fadeIn("left", 0.2)}
+            initial="hidden"
+            whileInView={"show"}
+            viewport={{ once: false, amount: 0.7 }}
+            className="flex-1 flex  flex-col gap-y-6"
+          >
+            <div className="group relative overflow-hidden border-2 border-white/50  rouderd-xl">
+              {/**  overloy */}
+              <div className="group-hover:bg-black/70 w-full h-full absolute z-40 transition-all duration-300"></div>
+              {/**  image */}
+              <img
+                className="group-hover:scale-125 transition-all duration-500"
+                src={Img1}
+                alt=""
+              />
+              {/**  pretitle */}
+              <div className="absolute  -bottom-full left-12 group-hover:bottom-24 transition-all duration-500  z-50">
+                <span className="text-gradient">UI/UX Desing</span>
+              </div>
+              {/**  title */}
+              <div className="absolute  -bottom-full left-12 group-hover:bottom-14 transition-all duration-700  z-50">
+                <span className="text-3xl text-white">Project Title</span>
+              </div>
+            </div>
+            <div className="group relative overflow-hidden border-2 border-white/50  rouderd-xl">
+              {/**  overloy */}
+              <div className="group-hover:bg-black/70 w-full h-full absolute z-40 transition-all duration-300"></div>
+              {/**  image */}
+              <img
+                className="group-hover:scale-125 transition-all duration-500"
+                src={Img3}
+                alt=""
+              />
+              {/**  pretitle */}
+              <div className="absolute  -bottom-full left-12 group-hover:bottom-24 transition-all duration-500  z-50">
+                <span className="text-gradient">UI/UX Desing</span>
+              </div>
+              {/**  title */}
+              <div className="absolute  -bottom-full left-12 group-hover:bottom-14 transition-all duration-700  z-50">
+                <span className="text-3xl text-white">Project Title</span>
+              </div>
+            </div>
+          </motion.div>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Work;
