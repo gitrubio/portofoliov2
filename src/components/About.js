be873c7e2ba3c529bbd382ca0ce3d232a05938b0
gitrubio <carloviloria0@gmail.com>
2024-01-12 21:28:48 -0500

    finish about section

--- src/components/About.js
+++ src/components/About.js
@@ -17,8 +17,18 @@ const About = () => {
     <section id="about" className="section" ref={ref}>
       <div className="container  mx-auto">
         <div className="flex flex-col gap-y-10 lg:flex-row  lg:items-center lg:gap-x-20  lg:gap-y-0  h-screen">
-          <div className="flex-1 bg-about bg-contain bg-no-repeat h-[640px] mix-blend-lighten bg-top"></div>
-          <div className="flex-1">
+          <motion.div
+          variants={fadeIn('right',0.3)}
+          initial='hidden'
+          whileInView={'show'}
+          viewport={{once: false, amount: 0.3}}
+          className="flex-1 bg-about bg-contain bg-no-repeat h-[640px] mix-blend-lighten bg-top"></motion.div>
+          <motion.div 
+           variants={fadeIn('left',0.5)}
+           initial='hidden'
+           whileInView={'show'}
+           viewport={{once: false, amount: 0.3}}
+          className="flex-1">
             <h2 className="h2 text-accent">About me.</h2>
             <h3 className="h3   mb-4">
               I'm a Freelance Full-stack Developer with over 2 years of
@@ -67,7 +77,7 @@ const About = () => {
                 My Portfolio
               </a>
             </div>
-          </div>
+          </motion.div>
         </div>
       </div>
     </section>
